298ec69312fc5db697dc97dc0e6bb50b7c58710b
TinaW00 <tina.wang@swivelsoftware.com>
2022-10-22 13:01:45 +0800

    menu add click function to link the template

--- src/components/ResultSetUI.vue
+++ src/components/ResultSetUI.vue
@@ -11,7 +11,7 @@
           <v-navigation-drawer id="resultset-submenu-navlist" permanent floating>
             <v-list dense nav>
               <v-list-item-group v-model="resultsetmenuindex" class="resultset-menu-nav-group" active-class="resultset-menu-nav-act">
-                <v-list-item v-for="(item, index) in resultsetmenu" :key="item.navname" link class="result-menu-nav">
+                <v-list-item v-for="(item, index) in resultsetmenu" :key="item.navname" @click="navtemplatelink(item.navname)" link class="result-menu-nav">
                   <v-list-item-icon>
                     <v-icon class="navicon">{{ item.icon }}</v-icon>
                   </v-list-item-icon>
@@ -22,7 +22,6 @@
                     <v-icon right class="resultset-menu-nav-arrow">mdi-arrow-right-thick</v-icon>
                   </v-list-item-icon>
                 </v-list-item>
-                
               </v-list-item-group>
             </v-list>
           </v-navigation-drawer>
@@ -53,26 +52,65 @@
         <div></div>
       </v-col>
       <v-col cols="10">
-        <template>
+        <template v-if="navtemplate == 'Property'">
           <v-tabs v-model="tabs" class="resultset-tabs" hide-slider background-color="transparent">
-              <v-tab v-for="item in resultsetbody" :key="item.tabname" class="resultset-tab"  active-class="resultset-tab-act">
-                <v-icon>{{ item.icon }}</v-icon>{{item.tabname}}
-              </v-tab>
-            </v-tabs>
+            <!-- <v-hover v-slot="{ hover }"> -->
+            <v-tab v-for="item in resultsetbody" :key="item.tabname" class="resultset-tab"  active-class="resultset-tab-act" :ripple="false">
+              <v-icon>{{ item.icon }}</v-icon>{{item.tabname}}
+            </v-tab>
+            <v-btn icon :ripple="false" plain style="margin:0 20px">
+              <v-icon>mdi-content-save-outline</v-icon>
+            </v-btn>
+            <!-- </v-hover> -->
+          </v-tabs>
+          
+          <v-tabs-items v-model="tabs" style="background: transparent;padding: 0 5px 20px 5px;">
             
-            <v-tabs-items v-model="tabs" style="background: transparent;">
-              <v-tab-item>
-              </v-tab-item>
+            <v-tab-item>
+              <v-card class="tab-property-card">
+                <v-list-item class="tab-property-card-content">
+                  <v-list-item-content>
+                      
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card>
+            </v-tab-item>
 
-              <v-tab-item>
-              </v-tab-item>
+            <v-tab-item>
+              <v-card class="tab-card">
+                <v-list-item class="tab-card-content">
+                  <v-list-item-content>
+                      <div class="tab-card-content-title"><v-icon left>mdi-menu</v-icon>SQL</div>
+                      <v-divider></v-divider>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card>
+              <v-card class="tab-card tab-card-JsonEditor">
+                <v-list-item class="tab-card-content">
+                  <v-list-item-content>
+                      <div class="tab-card-content-title"><v-icon left>mdi-menu</v-icon>Ask</div>
+                      <v-divider></v-divider>
+                      <v-data-table :headers="JsonEditorHeaders" :items="JsonEditorItem" item-key="name" :items-per-page="5">
+                        <!-- <template v-slot:item.calories="{ item }">
+                          <v-icon :color="getColor(item.calories)" dark>
+                            {{ item.calories }}
+                          </v-icon>
+                        </template> -->
+                      </v-data-table>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-card>
+            </v-tab-item>
+          
+            <v-tab-item>
+            </v-tab-item>
 
-              <v-tab-item>
-              </v-tab-item>
+            <v-tab-item>
+            </v-tab-item>
+          </v-tabs-items>
+        </template>
 
-              <v-tab-item>
-              </v-tab-item>
-            </v-tabs-items>
+        <template v-if="navtemplate == 'Browse'">
         </template>
       </v-col>
 
@@ -85,13 +123,79 @@
 <script>
   export default {
     data: () => ({
-      resultsetmenu:[{navname:'Property',icon:'mdi-cog'},{navname:'Browse',icon:'mdi-table-large'}],
+      tabs: 1,
+      resultsetmenu:[{navname:'Property',icon:'mdi-cog',link:'https://github.com/vuetifyjs/vuetify'},{navname:'Browse',icon:'mdi-table-large',link:'https://github.com/vuetifyjs/vuetify'}],
       resultsetcomponent:[],
       resultsetmenuindex:0,
       resultsetbody:[{tabname:'Property',icon:'mdi-cog'},{tabname:'SQL',icon:'mdi-database'},{tabname:'DATAMODEL',icon:'mdi-database'}],
+      navtemplate:'Property',
+      JsonEditorHeaders: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          value: 'btn',
+        },
+        { text: 'Key', value: 'key' },
+        { text: 'Title', value: 'title' },
+        { text: 'Data Type', value: 'datatype' },
+        { text: 'Test Value', value: 'testvalue' },
+    ],
+    JsonEditorItem: [
+      {
+        btn: '',
+        key: 'xvdate1',
+        title: 'Date',
+        datatype: 'Date',
+        testvalue: '2021/01/01',
+      },
+      {
+        btn: '',
+        key: 'xvdate1',
+        title: 'Date',
+        datatype: 'Date',
+        testvalue: '2021/01/01',
+      },
+      {
+        btn: '',
+        key: 'xvdate1',
+        title: 'Date',
+        datatype: 'Date',
+        testvalue: '2021/01/01',
+      },
+      {
+        btn: '',
+        key: 'xvdate1',
+        title: 'Date',
+        datatype: 'Date',
+        testvalue: '2021/01/01',
+      },
+      {
+        btn: '',
+        key: 'xvdate1',
+        title: 'Date',
+        datatype: 'Date',
+        testvalue: '2021/01/01',
+      },
+      {
+        btn: '',
+        key: 'xvdate1',
+        title: 'Date',
+        datatype: 'Date',
+        testvalue: '2021/01/01',
+      },
+      {
+        btn: '',
+        key: 'xvdate1',
+        title: 'Date',
+        datatype: 'Date',
+        testvalue: '2021/01/01',
+      },
+    ],
     }),
     methods:{
-      
+      navtemplatelink(temp){
+        this.navtemplate = temp;
+      }
     }
   }
 </script>
@@ -124,12 +228,7 @@
   --Menu-Dashboard-icon-color :#A0B2B6;
   --System-Top-select--comma-color :#A0B2B6;
 }
-*{
-  font-family: 'inter';
-}
-.v-main__wrap{
-  background:#f5f5f5;
-}
+
 #main{
   position: relative;
   height: calc(100vh - 49px);
@@ -224,9 +323,13 @@
   padding: 0;
 }
 
+.resultset-tabs .v-slide-group__content {
+    justify-content: end;
+    align-items: center;
+}
 .resultset-tab {
     /* color: #000!important; */
-    background: transparent!important;
+    background: transparent;
     margin: 0 0px!important;
     padding: 12px 15px;
     z-index: 1;
@@ -234,15 +337,16 @@
     position: relative;
     display: flex;
     align-items: center;
+    font-weight: medium;
 }
-.resultset-tab::before {
+.resultset-tab:before {
     content: "";
     position: absolute;
     right: 0;
     bottom: 0;
     left: 0;
     z-index: -1;
-    background: #EBEBEB!important;
+    background: #ebebeb!important;
     border-radius: 1.5em 1.5em 0 0;
     transform: scale(1.08) perspective(1.8em) rotateX(4deg);
     transform-origin: bottom;
@@ -250,20 +354,74 @@
     opacity: 1;
     height: 55px;
 }
-
-.resultset-tab-act::before{
+.resultset-tab-act{
+  z-index: 2;
+  color: #000!important;
+}
+.resultset-tab-act:before{
   content: "";
   position: absolute;
   right: 0;
   bottom: 0;
   left: 0;
   z-index: -1;
-  background: #000!important;
-  z-index: 1;
-}
-.resultset-tab-act:before{
-  opacity:1!important;
+  background: #fff!important;
   z-index: -1;
+  opacity:1!important;
+}
+.theme--light.v-tabs .v-tab:hover::before {
+    /* opacity: 0.6;
+    background: #fff!important; */
+}
+.resultset-tabs .resultset-tab:hover::before {
+    opacity: 0.6!important;
+    background: #fff!important;
+}
+
+.tab-property-card{
+  font-size: 16px;
+  min-height: 20vh;
+  box-shadow: 0px 3px 18px rgba(68,67,67,.05)!important;
+  background: #fff!important;
+  border-radius: 12px!important;
+}
+.tab-property-card-content{
+  padding: 0;
+}
+.tab-card{
+  font-size: 16px;
+  min-height: 20vh;
+  box-shadow: 0px 3px 18px rgba(68,67,67,.05)!important;
+  background: #fff!important;
+  border-radius: 12px!important;
+  margin-bottom: 15px;
+}
+.tab-card-JsonEditor{
+  min-height: fit-content;
+}
+.tab-card-content{
+  padding: 0;
+}
+.tab-property-card-content .v-icon{
+  color:#A0B2B6;
+}
+.tab-card-content-title{
+  font-size: 20px;
+  padding: 5px 20px;
+  margin-bottom: 0;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+}
+.tab-card .v-list-item{
+  min-height: auto;
+}
+.tab-card .v-list-item__content{
+  padding: 0px!important;
+  margin: 0;
+}
+.tab-card .v-list-item__content > *:not(:last-child){
+  margin: 0;
 }
 
 </style>
